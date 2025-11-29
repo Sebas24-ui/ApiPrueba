90e7427a4fb3d89340a462afc082e62648e58ab2
Sebastián Albacura <sebastianalbacura24@gmail.com>
2025-11-29 11:45:58 -0500

    hola

--- Prueba_Clases/ApiResult.cs
+++ Prueba_Clases/ApiResult.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Prueba_Clases;
 namespace Prueba_Clases
 {
     public class ApiResult<T>
