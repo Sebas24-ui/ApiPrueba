0c4abd065888f522693a6875f8ad7b891aa40588
Sebastián Albacura <sebastianalbacura24@gmail.com>
2025-11-29 12:06:04 -0500

    hola

--- ApiPrueba/Controllers/RazasController.cs
+++ ApiPrueba/Controllers/RazasController.cs
@@ -9,7 +9,7 @@ using Prueba_Clases;
 
 namespace ApiPrueba.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]")] 
     [ApiController]
     public class RazasController : ControllerBase
     {
